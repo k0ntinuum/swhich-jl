32f3c77e68399d76e9032d0cda55a037edb96a39
k0ntinuum <k0ntinuum@proton.me>
2023-11-06 03:39:08 -0500

    a

--- swhich-jl.jl
+++ swhich-jl.jl
@@ -7,8 +7,9 @@ end
 
 
 function str(v,s)
-    #out_alph = "abcdefghijklmnopqrstuvwxyz_"
-    out_alph = "O|"
+    #out_alph = "abcdefghijklmnopqrstuvwxyz" * string(\u2586)    
+    out_alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" * string('\u25A0')
+    #out_alph = "O|"
     join(map(i -> out_alph[i+1:i+1] * s,v ))
 end
 rgb(r,g,b) =  "\e[38;2;$(r);$(g);$(b)m"
@@ -34,24 +35,24 @@ end
 
 
 
-function encode(p,q,n)
-    f = deepcopy(q)
+function encode!(p,f,n)
     c = Int64[]
     for i in eachindex(p)
         push!(c, ( f[1][1] + f[2][1] + p[i] )%n )
         swhich!(f)
         f[1][1] = (f[1][1] + p[i])%n
+        f[2][1] = (f[2][1] + c[i])%n
     end
     c
 end
 
-function decode(c,q,n)
-    f = deepcopy(q)
+function decode!(c,f,n)
     p = Int64[]
     for i in eachindex(c)
         push!(p, ( 2*n + c[i]  - f[1][1] - f[2][1])%n )
         swhich!(f)
         f[1][1] = (f[1][1] + p[i])%n
+        f[2][1] = (f[2][1] + c[i])%n
     end
     p
 end
@@ -60,8 +61,9 @@ function encrypt(p,q,n)
     l = length(q)
     for i in 1:l
         f = deepcopy(q)
-        f = circshift(f,i)
-        p = encode(p,f,n)
+        f[1] = circshift(f[1],i)
+        f[2] = circshift(f[2],l + 1 - i)
+        p = encode!(p,f,n)
         p = reverse(p)
     end
     p
@@ -70,32 +72,23 @@ end
 function decrypt(c,q,n)
     l = length(q)
     for i in 1:l
-        f = circshift(q, l + 1 - i)
+        f = deepcopy(q)
+        f[2] = circshift(f[2],i)
+        f[1] = circshift(f[1],l + 1 - i)
         c = reverse(c)
-        c = decode(c,f,n)
+        c = decode!(c,f,n)
     end
     c
 end
 
-function swhich_demo(n)
-    l = 40
-    f = key(n,l)
-    print_key(f)
-    for i in 2:l
-        swhich!(f)
-        print_key(f)
-    end
-end
-
-
 
 function demo(n)
-    l = 32
+    l = 64
     f = key(n,l)
     print_key(f)
     println()
     for i in 1:10
-        p = rand(0:n-1,2*l)
+        p = rand(0:n-1,l)
         print(red())
         print_vec(p)
         c = encrypt(p,f,n)
